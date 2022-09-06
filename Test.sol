9d0ce49e63d2c2c86d8d7469b02ee53816da1294
Rohit Kumar <67726628+rohitverse@users.noreply.github.com>
2022-09-06 20:55:12 +0530

    Add files via upload

---
+++ Test.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier:MIT;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract test{
+    uint256 public age=10;
+ function getter(uint256 newage) public{
+     age=newage;
+ }
+}
